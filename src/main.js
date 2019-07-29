756c7daec1eda7e8ea2a68449b4ecbc22526e583
wingever <2235614169@qq.com>
2019-07-29 11:29:59 +0800

    “first

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(<App/>, document.getElementById('app'))
